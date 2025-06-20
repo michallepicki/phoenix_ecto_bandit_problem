0a2caa13141b2741ccd94989834d20d6dcd54a0a
Michał Łępicki <michallepicki@users.noreply.github.com>
2025-06-20 21:03:57 +0200

    install req

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule Hello.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:req, "~> 0.5.0"}
     ]
   end
 
